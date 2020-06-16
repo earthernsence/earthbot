bb84bd888d8f1cc56a2fd512b820df5885e263e0
earth <divine.icbm@gmail.com>
2020-06-16 12:01:02 -0500

    Add files via upload

--- earthbot.js
+++ earthbot.js
@@ -1,25 +1,37 @@
-/* eslint-disable no-inline-comments */
+/* eslint-disable max-len */
+/* eslint-disable no-debugger */
 /* eslint-disable no-console */
 /* eslint-disable camelcase */
 "use strict";
 
 const Discord = require("discord.js");
+const { isUndefined } = require("util");
 const client = new Discord.Client();
-const token /* Your token here */ = "";
+const token = "";
 let isRankEnabled = true;
-let date = Date.now();
+const date = Date.now();
 
 client.on("ready", () => {
-    const mainChat = client.channels.cache.get(/* channel id */);
-    mainChat.send("I have a dream");
+    const mainChat = client.channels.cache.get("722486630367363112");
+    mainChat.send(`Bot is online at ${new Date(Date.now())}`);
 });
 
 client.on("message", message => {
+    if (isUndefined(message.author.diffTotal) || isNaN(message.author.diffTotal)) message.author.diffTotal = 60;
+    setInterval(increaseDiff, 1000);
+    if (isUndefined(message.author.experience)) message.author.experience = 0;
+    if (isUndefined(message.author.messagesCountedForExperience)) message.author.messagesCountedForExperience = 0;
     if (isNaN(message.author.messageCount)) message.author.messageCount = 0;
+    if (isNaN(message.author.experience)) message.author.experience = 0;
     message.author.messageCount++;
-    gainExperience(message.author);
     console.log(date);
-    if (!message.content.startsWith("++") || message.author.bot) {
+    if (!message.content.startsWith("++") || !message.author.bot) {
+        gainExperience(message.author);
+    } else if (message.content === "good bot") {
+        message.channel.send("Aww, thank you! <3");
+    } else if (message.content === "bad bot") {
+        message.channel.send("No need to be so harsh!");
+    } else if (message.author.bot) {
         return;
     }
     if (message.content === "++rank" && isRankEnabled) {
@@ -27,9 +39,11 @@ client.on("message", message => {
         Message count: ${message.author.messageCount}
         Messages counted for experience: ${message.author.messagesCountedForExperience}
 
-        Rank: Not coded
+        Rank: ${message.author.level} (${(5 * (Math.pow(message.author.level, 1.5)) + 50 * message.author.level + 100) - message.author.experience} until next level)
         Experience: ${message.author.experience}
         `);
+    } else if (message.content === "++rank" && !isRankEnabled) {
+        message.channel.send(`Ranking is currently disabled.`);
     }
     if (message.content === "++rank toggle") {
         isRankEnabled = !isRankEnabled;
@@ -37,24 +51,34 @@ client.on("message", message => {
         console.log(`Ranking is now set to ${isRankEnabled}`);
         console.log(message.author);
     }
-    //
-    // gainExperience(message.author);
+    if (message.content === "++diff") {
+        message.channel.send(`${message.author.diffTotal}`);
+    }
+    function increaseDiff() {
+        message.author.diffTotal++;
+    }
 });
+
  
  function gainExperience(user) {
      if (!isRankEnabled) return;
      if (user.bot) return;
-     user.lastGainedExperience = date + 60000;
+     if (user.diffTotal <= 59) return;
+
      if (isNaN(user.level)) user.level = 0;
      if (isNaN(user.experience)) user.experience = 0;
-     console.log(`Experience for ${user}: ${user.experience}`);
      if (isNaN(user.messagesCountedForExperience)) user.messagesCountedForExperience = 0;
-     let diff = 60000;
 
-     if (diff >= 60000) {
-     diff = 0;
+     if (user.diffTotal >= 60) {
+     user.diffTotal = 0;
      user.lastGainedExperience = user.date;
-     user.experience = (Math.random() * 10) + 10;
+     user.experience += Math.floor((Math.random() * 10) + 10);
+     if (user.experience >= 5 * (Math.pow(user.level, 1.5)) + 50 * user.level + 100) {
+        user.level++;
+        user.experience = 0;
+        user.channel.send(`Congratulations! You ranked up to ${user.level}. You need ${(5 * (Math.pow(message.author.level, 1.5)) + 50 * message.author.level + 100)} experience for the next level.`);
+     }
+     console.log(`Experience for ${user}: ${user.experience}`);
 
      // Level formula: 5 * (Math.pow(user.level, 1.5)) + 50 * user.level + 100
 
